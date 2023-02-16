69deb6886c16aa73c6509f7af382ec8c4722477d
Jelte Dirks <j.dirks@outlook.com>
2023-02-16 22:42:59 +0100

    basic prompt for some choices, with a predefined struct to choose from
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,87 @@
-fn main() {
-    let stdin = std::io::stdin();
-    let mut input = String::new();
-    stdin.read_line(&mut input).expect("error while reading input from stdin");
-
-    let as_bool = string_to_boolean(&input);
-
-    if let Ok(b) = as_bool {
-        if b {
-            println!("{}", input);
-        } else {
-            println!("na");
-        }
-    } else {
-        // something went wrong in the conversion
-        println!("{:?}", as_bool.unwrap_err());
-    }
-}
+use std::io::stdin;
+use std::io::stdout;
+use std::process::exit;
 
 #[derive(Debug)]
 enum CLIErrors {
     CouldNotParse,
+}
+
+fn main() {
+    let mut input = String::new();
+    let mut type_option = OptionPrompt {
+        query: "What type is the label".to_string(),
+        options: Vec::new(),
+    };
+
+    type_option.options.push(String::from("basis"));
+    type_option.options.push(String::from("tekst"));
+    type_option.options.push(String::from("label"));
+    type_option.options.push(String::from("valuta"));
+
+    let prompt_result = prompt(&type_option);
+
+    println!("prompt result {}", prompt_result);
+
+    loop {
+        stdin()
+            .read_line(&mut input)
+            .expect("error while reading input from stdin");
+
+        let as_bool = string_to_boolean(&input);
+
+        if let Ok(b) = as_bool {
+            if b {
+                println!("{}", input);
+            } else {
+                println!("na");
+            }
+        } else {
+            // something went wrong in the conversion
+            println!("{:?}", as_bool.unwrap_err());
+            exit(1);
+        }
+
+        input.clear();
+    }
+}
+
+struct OptionPrompt {
+    query: String,
+    options: Vec<String>,
+}
+
+fn prompt(prompt: &OptionPrompt) -> String {
+    loop {
+        let mut i = 0;
+        for option in &prompt.options {
+            println!("({}) {:?}", i, option);
+            i += 1;
+        }
+        println!("{}", prompt.query);
+
+        let mut response = String::new();
+
+        stdin()
+            .read_line(&mut response)
+            .expect("error reading input from stdin");
+
+        let index = str::parse::<usize>(&response.trim());
+
+        if index.is_err() {
+            println!("could not parse that choice, try again");
+            continue;
+        }
+
+        let result_option = prompt.options.get(index.unwrap());
+
+        match result_option.is_some() {
+            true => return result_option.unwrap().to_string(),
+            false => {
+                println!("something went wrong or that choice is invalid, try again");
+            }
+        }
+    }
 }
 
 fn string_to_boolean(value: &String) -> Result<bool, CLIErrors> {
@@ -43,4 +105,3 @@ fn string_to_boolean(value: &String) -> Result<bool, CLIErrors> {
 
     return Err(CLIErrors::CouldNotParse);
 }
-
